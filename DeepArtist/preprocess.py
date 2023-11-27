e544ed72d1986c71b9ddef320e798315c680cd82
Will Brandon <will17brandon@icloud.com>
2023-11-27 16:28:56 -0500

    loading and splitting done

--- DeepArtist/preprocess.py
+++ DeepArtist/preprocess.py
@@ -10,23 +10,18 @@ from torch.utils.data import Subset
 from sklearn.model_selection import train_test_split
 
 
-ROOT = './DATA'
-
 class LabeledImageDataset(ImageFolder):
 
     def __init__(self, root_dir: str, transform) -> None:
         super().__init__(root_dir, transform)
-
-
-    def labels(self) -> list[str]:
-        return self.targets
-
-
-    def length(self) -> int:
-        return len(self.labels())
-
+    
 
     def label_map(self) -> list[str]:
+        '''
+        Returns a list of the label (target) name strings originating from the directory name the
+        images were stored in. This computes the list upon each call so this should be called once
+        and the result should be stored.
+        '''
 
         def extract_label(label_tuple: str) -> str:
             return os.path.basename(os.path.dirname(label_tuple[0]))
@@ -42,47 +37,83 @@ class LabeledImageDataset(ImageFolder):
         return label_map
 
 
+def split_dataset(dataset,
+                  train_split: float=0.8,
+                  validate_split: float=0.1,
+                  random_seed: int=None):
+    '''
+    Splits a given dataset into 3 subsets: train, validate, and test. The train_split and
+    validate_split portions must add up to no more than 1.0. The size of the test set will be
+    1.0 - train_split - validate_split.
+    '''
 
+    if train_split + validate_split > 1.0:
+        raise ValueError(
+            'The portion size of the train and validate splits must sum up to no more than 1.0.')
+
+    dataset_all_indices = list(range(len(dataset)))
+    train_idx, posttrain_idx = train_test_split(dataset_all_indices, 
+                                                test_size=train_split,
+                                                shuffle=True,
+                                                random_state=random_seed)
+    
+    posttrain_dataset = Subset(dataset, posttrain_idx)
+    posttrain_all_indices = list(range(len(posttrain_dataset)))
+    validate_idx, test_idx = train_test_split(posttrain_all_indices,
+                                              test_size=validate_split / (1 - train_split),
+                                              shuffle=True,
+                                              random_state=random_seed)
+
+    return (
+        Subset(dataset, train_idx),
+        Subset(posttrain_dataset, validate_idx),
+        Subset(posttrain_dataset, test_idx)
+    )
+
+
+DATA_ROOT = './Data'
 
 transform = transforms.Compose([
     transforms.ToImage(),
-    transforms.Resize((224, 224), antialias=True),  # Explicitly set antialias to True
     transforms.ToDtype(torch.float32, scale=True),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Adjust if images are grayscale
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
 
-def split_dataset(dataset, train_proportion=0.8, validate_size=0.1):
-    indices = list(range(len(dataset)))
-    train_idx, posttrain_idx = train_test_split(indices, test_size=train_proportion)
-    posttrain_dataset = Subset(dataset, posttrain_idx)
-
-    indices = list(range(len(posttrain_dataset)))
-
-    validate_idx, test_idx = train_test_split(indices, test_size=validate_size / (1 - train_proportion))
-
-    datasets = {}
-    datasets['train'] = Subset(dataset, train_idx)
-    datasets['validation'] = Subset(posttrain_dataset, validate_idx)
-    datasets['test'] = Subset(posttrain_dataset, test_idx)
-
-    return datasets
-
-
-def load(root_dir: str, batch_size: int = 100):
-
-    dataset = LabeledImageDataset(root_dir=root_dir, transform=transform)
-
+def load_artelligence_data(batch_size: int=100,
+                           train_split: float=0.8,
+                           validate_split: float=0.1,
+                           random_seed: int=None):
+    
+    dataset = LabeledImageDataset(root_dir=DATA_ROOT, transform=transform)
     label_map = dataset.label_map()
 
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+    print(len(dataset))
 
-    datatests = split_dataset(dataset)
+    train_dataset, validate_dataset, test_dataset = split_dataset(dataset,
+                                                                  train_split,
+                                                                  validate_split,
+                                                                  random_seed)
+    
+    print(len(train_dataset))
 
-    print(datatests['train'].targets())
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
+    validate_loader = DataLoader(validate_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
-    return loader
+    return train_loader, validate_loader, test_loader, label_map
+
 
 if __name__ == '__main__':
+    (
+        train_loader,
+        validate_loader,
+        test_loader,
+        label_map
+    ) = load_artelligence_data(batch_size=100, train_split=0.8, validate_split=0.1, random_seed=1)
 
-    loader = load(ROOT)
+
+    print(len(train_loader))
+    print(len(validate_loader))
+    print(len(test_loader))
+    print(label_map)
