b0c9e42b38ef973e9683f0be638cccd003f5d1f5
evansuslovich <evanjacobsuslovich@gmail.com>
2023-11-28 15:13:07 -0500

    download data, train model, save model, upload model

--- DeepArtistApp/backend/base.py
+++ DeepArtistApp/backend/base.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
+import torch
+import torchvision
+import torchvision.transforms.v2 as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 CORS(app)
@@ -16,27 +23,109 @@ def allowed_file(filename):
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    print(request.data)
-    print(request.form)
-    print(request)
 
-    return jsonify({'message': 'File Uploaded Succesfully'})
-    # # Check if the POST request has a file part
-    # if 'file' not in request.files:
-    #     return jsonify({'error': 'No file part'})
+    # Check if the POST request has a file part
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'})
 
-    # file = request.files['file']
+    file = request.files['file']
 
-    # # Check if the file is allowed and not empty
-    # if file.filename == '' or not allowed_file(file.filename):
-    #     return jsonify({'error': 'Invalid file'})
+    # Check if the file is allowed and not empty
+    if file.filename == '' or not allowed_file(file.filename):
+        return jsonify({'error': 'Invalid file'})
 
-    # # Save the file to the upload folder
-    # if file:
-    #     filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    #     file.save(filename)
-    #     return jsonify({'message': 'File uploaded successfully'})
 
+# PART 1: Preprocess the fashion mnist dataset and determine a good batch size for the dataset.
+transform = transforms.Compose([
+    transforms.ToImage(),
+    transforms.ToDtype(torch.float32, scale=True),
+    transforms.Grayscale(1), # Merge all 3 channels (that should already be equal) to grayscale using mean.
+    transforms.Normalize(torch.tensor([0]), torch.tensor([0.5]))
+])
+batch_size = 64
+
+# PART 2: Load the dataset. Make sure to utilize the transform and batch_size.
+trainset = torchvision.datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+testset = torchvision.datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+
+# PART 3: Design a multi-layer perceptron.
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(784, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 10)
+
+    # bias terms: 
+    # ((784 * 512) + 512) + ((512 * 256)  + 256) + ((256 * 10) + 10)
+    def forward(self, x):
+        x = x.view(-1, 28 * 28)  # Flatten the input
+        x = F.relu(self.fc1(x)) # run through relu 
+        x = F.relu(self.fc2(x)) # run through relu 
+        x = self.fc3(x)
+        return x
+
+net = Net()
+
+def train_model(): 
+    # PART 4: Choose a good loss function and optimizer.
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(net.parameters(), lr=0.001)
+
+    # PART 5: Train your model!
+    num_epochs = 15
+
+    training_losses = []
+
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for i, data in enumerate(trainloader, 0):
+            inputs, labels = data
+            optimizer.zero_grad()
+            outputs = net(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+        training_losses.append(running_loss / len(trainloader))
+        print(f"Epoch {epoch + 1}, Training loss: {training_losses[-1]}")
+
+    torch.save(net.state_dict(), "model.pth")
+    print('Finished Training')
+
+def evaluate_model():
+    # PART 6: Evaluate your model!
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = 100 * correct / total
+    print('Accuracy: {:.2f}%'.format(accuracy))
+
+
+try:
+    # Test the model
+    net.load_state_dict(torch.load('model.pth', map_location="cpu"))
+    # net.to(device)
+    print("Model Loaded from './model_training.pth'")
+    evaluate_model()
+except Exception as  e: 
+    print(e)
+    print("Training Model")
+    train_model()
+    print("Evaluating Model")
+    evaluate_model()
 
 if __name__ == '__main__':
     app.run(debug=True)
