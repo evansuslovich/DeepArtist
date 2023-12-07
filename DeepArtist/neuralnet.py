2f2caa4478b943942e1f5053260fc2d56d6f2dee
Will Brandon <will17brandon@icloud.com>
2023-12-07 17:42:45 -0500

    got 65% model

--- DeepArtist/neuralnet.py
+++ DeepArtist/neuralnet.py
@@ -2,7 +2,6 @@ import os
 from collections import OrderedDict
 from tqdm import tqdm
 
-import numpy as np
 from matplotlib import pyplot as plt
 from preprocess import ImageDataManager
 
@@ -14,7 +13,8 @@ import torch.optim as optim
 MODEL_STATE_DICT_FILE = 'model.pth'
 BATCH_SIZE = 64
 SQUARE_IMAGE_SIZE = 100
-TRAINING_EPOCHS = 3
+TRAINING_EPOCHS = 10
+DEBUG_PRINT_SIZE = False
 
 
 if torch.backends.mps.is_available():
@@ -27,7 +27,6 @@ else:
 print(f'Usinge device: {device}')
 
 
-
 idm = ImageDataManager(data_root='./Data', square_image_size=SQUARE_IMAGE_SIZE)
 
 (
@@ -38,6 +37,21 @@ idm = ImageDataManager(data_root='./Data', square_image_size=SQUARE_IMAGE_SIZE)
 
 print(f"SIZES: (train={train_size}, validate={validate_size}, test={test_size})")
 
+
+class DebugPrintSize(nn.Module):
+
+    def __init__(self, label):
+        super(DebugPrintSize, self).__init__()
+
+        self.label = label
+    
+    
+    def forward(self, x):
+
+        if DEBUG_PRINT_SIZE:
+            print(f'{self.label}: {x.size()}')
+        
+        return x
 
 
 class Net(nn.Module):
@@ -62,40 +76,67 @@ class Net(nn.Module):
     def build_convolutional_layers(self):
         self.convolution_model = nn.Sequential(OrderedDict([
 
+            ('print_size1',       DebugPrintSize('Step 0')),
             ('conv1',             nn.Conv2d(3, 16, self.kernel_size, padding=self.padding_size)),
+            ('print_size2',       DebugPrintSize('Step 1')),
             ('inner_activation1', self.inner_activation),
+            ('print_size3',       DebugPrintSize('Step 2')),
             ('pool1',             self.pool),
 
+            ('print_size4',       DebugPrintSize('Step 3')),
             ('conv2',             nn.Conv2d(16, 32, self.kernel_size, padding=self.padding_size)),
+            ('print_size5',       DebugPrintSize('Step 4')),
             ('inner_activation2', self.inner_activation),
+            ('print_size6',       DebugPrintSize('Step 5')),
             ('pool2',             self.pool),
+            ('print_size7',       DebugPrintSize('Step 6')),
 
+            ('print_size8',       DebugPrintSize('Step 7')),
             ('conv3',             nn.Conv2d(32, 64, self.kernel_size, padding=self.padding_size)),
+            ('print_size9',       DebugPrintSize('Step 8')),
             ('inner_activation3', self.inner_activation),
+            ('print_size10',      DebugPrintSize('Step 9')),
             ('pool3',             self.pool),
 
+            ('print_size11',      DebugPrintSize('Step 10')),
             ('conv4',             nn.Conv2d(64, 128, self.kernel_size, padding=self.padding_size)),
+            ('print_size12',      DebugPrintSize('Step 11')),
             ('inner_activation4', self.inner_activation),
+            ('print_size13',      DebugPrintSize('Step 12')),
             ('pool4',             self.pool),
 
+            ('print_size14',      DebugPrintSize('Step 13')),
             ('conv5',             nn.Conv2d(128, 256, self.kernel_size, padding=self.padding_size)),
+            ('print_size15',      DebugPrintSize('Step 14')),
             ('inner_activation5', self.inner_activation),
+            ('print_size16',      DebugPrintSize('Step 15')),
             ('pool5',             self.pool),
+            
+            ('print_size17',      DebugPrintSize('Step 16'))
         ]))
+
 
     def build_flat_layers(self):
 
         # The flat part of the model consists of three linear layers.
         self.flat_model = nn.Sequential(OrderedDict([
             
+            ('print_size1',       DebugPrintSize('Step 17')),
             ('linear1',           nn.Linear(2304, 512)),
+            ('print_size2',       DebugPrintSize('Step 18')),
             ('inner_activation1', self.inner_activation),
 
+            ('print_size3',       DebugPrintSize('Step 19')),
             ('linear2',           nn.Linear(512, 256)),
+            ('print_size4',       DebugPrintSize('Step 20')),
             ('inner_activation2', self.inner_activation),
 
+            ('print_size5',       DebugPrintSize('Step 21')),
             ('linear3',           nn.Linear(256, 5)),
-            ('final_activation3', self.final_activation)
+            ('print_size6',       DebugPrintSize('Step 22')),
+            ('final_activation3', self.final_activation),
+
+            ('print_size7',       DebugPrintSize('Step 23'))
         ]))
     
 
@@ -104,7 +145,7 @@ class Net(nn.Module):
         # Perform a pass through the convolitional layers, then flatten the data, then pass through
         # the flat layers.
         x = self.convolution_model(x)
-        x = x.view(-1, 256 * 3 * 3)
+        x = x.view(-1, 2304)
         x = self.flat_model(x)
         
         # Yield the result after having passed through the entire model.
@@ -129,7 +170,7 @@ else:
 
     net.train()
 
-    for epoch in range(TRAINING_EPOCHS):
+    for epoch in range(1, TRAINING_EPOCHS + 1):
         print("EPOCH: ", epoch)
         running_loss = 0.0
         for data in tqdm(train_loader):
@@ -151,6 +192,14 @@ else:
     print('Finished Training')
     torch.save(net.state_dict(), MODEL_STATE_DICT_FILE)
 
+    plt.style.use('dark_background')
+
+    loss_plot_fig, loss_plot_ax = plt.subplots()
+    loss_plot_x = torch.arange(0, TRAINING_EPOCHS, 1)
+    loss_plot_ax.plot(loss_plot_x, losses, c='blue')
+    loss_plot_ax.set(title='Cross-Entropy Loss By Epoch', xlabel='Epoch', ylabel='Cross-Entropy Loss')
+    loss_plot_fig.savefig(f'loss_by_epoch.png')
+
 
 # Test the model
 net.load_state_dict(torch.load(MODEL_STATE_DICT_FILE, map_location="cpu"))
@@ -161,7 +210,7 @@ correct=[]
 
 net.eval()
 accuracy = 0
-for inputs, labels in test_loader:
+for inputs, labels in tqdm(test_loader):
 	inputs, labels = inputs.to(device), labels.to(device)
 	outputs = net(inputs)
 	_, predicted = torch.max(outputs.data, 1)
@@ -169,14 +218,5 @@ for inputs, labels in test_loader:
 	correct.append((predicted == labels).tolist())
 
 print('Accuracy of the network on the test images: %d %%' % (100 * accuracy / test_size))
-
-
-plt.style.use('dark_background')
-
-loss_plot_fig, loss_plot_ax = plt.subplots()
-loss_plot_x = torch.arange(0, TRAINING_EPOCHS, 1)
-loss_plot_ax.plot(loss_plot_x, losses, c='blue')
-loss_plot_ax.set(title='Cross-Entropy Loss By Epoch', xlabel='Epoch', ylabel='Cross-Entropy Loss')
-loss_plot_fig.savefig(f'loss_by_epoch.png')
 
 plt.show()
